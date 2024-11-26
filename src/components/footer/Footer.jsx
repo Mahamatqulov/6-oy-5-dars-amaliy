3d67ee04e5af3f34a5619878388b6c4c977a7ee5
bahrom <baxrom@email.com>
2024-11-26 15:25:17 +0500

    CUser ready

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+// style
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <p>All rights reserved.</p>
+        <a href="https://www.t.me/BahromWEB">Bahrom</a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
